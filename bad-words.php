5f0e784b11fde5b5dcf51a8d82cdbd5665a8b469
plastikSun <marcom2k01@gmail.com>
2023-09-25 17:16:18 +0200

    edit: echo concat syntax

--- bad-words.php
+++ bad-words.php
@@ -2,4 +2,8 @@
 $query = $_POST['sentence'];
 $bad_words = ['composto', 'questo', 'giogo', 'problematica', 'completo'];
 
-echo $query, ' | (Lunghezza: ' . strlen($query) . ')';
+// echo $query, ' | (Lunghezza: ' . strlen($query) . ')';
+
+// echo "{$query} | (Lunghezza: {strlen($query)}) ";
+echo "{$query} | (Lunghezza: ". strlen($query) .")";
+
