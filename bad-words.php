dccf6f60625570b3685ea707a11c5aac3d2d9008
plastikSun <marcom2k01@gmail.com>
2023-09-25 17:31:50 +0200

    add: str_replce to filter sentence with $filter POST variable

--- bad-words.php
+++ bad-words.php
@@ -4,6 +4,8 @@ $filter = $_POST['filter'];
 
 echo nl2br("{$query} | (Lunghezza: ". strlen($query) .")\n");
 
-$filteredSentence = str_replace($query,'***',$filter);
-echo $filteredSentence;
+$filteredSentence = str_replace($filter,'***',$query);
+echo "Frase filtrata: {$filteredSentence}";
+
+
 
