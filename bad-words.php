131aaae4bd91895ce7841aff41bde37a211f9a23
plastikSun <marcom2k01@gmail.com>
2023-09-25 17:28:26 +0200

    add: linebreak at line 5 (nl2br)

--- bad-words.php
+++ bad-words.php
@@ -2,5 +2,8 @@
 $query = $_POST['sentence'];
 $filter = $_POST['filter'];
 
-echo "{$query} | (Lunghezza: ". strlen($query) .")";
+echo nl2br("{$query} | (Lunghezza: ". strlen($query) .")\n");
+
+$filteredSentence = str_replace($query,'***',$filter);
+echo $filteredSentence;
 
